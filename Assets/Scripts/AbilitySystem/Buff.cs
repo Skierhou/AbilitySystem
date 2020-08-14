f5034fd9468c28153021536a0a1da9cdc03745d6
Skierhou <675363508@qq.com>
2020-08-14 18:34:31 +0800

    上传

---
+++ Assets/Scripts/AbilitySystem/Buff.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EBuffType
+{
+    EBT_None,
+};
+public enum EBuffTag
+{
+    EBT_Metal,
+    EBT_Wood,
+    EBT_Water,
+    EBT_Fire,
+    EBT_Earth,
+};
+
+public class Buff
+{
+    private EBuffType buffType;
+    private EBuffTag buffTag;           //自身标签
+    private EBuffTag buffImmuneTag;     //免疫标签
+
+    public bool bNoCaster;
+    private GameObject caster;
+    private GameObject target;
+    private Ability ability;
+
+    private int layer;
+    private int level;
+    private float druation;
+
+    public object content;
+
+    public GameObject Caster { get => bNoCaster ? null : caster; set => caster = value; }
+}
---
+++ Assets/Scripts/AbilitySystem/Skill.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Skill : Ability
+{
+    public Skill(AbilitySystemComponent abilitySystem) : base(abilitySystem) { }
+}
